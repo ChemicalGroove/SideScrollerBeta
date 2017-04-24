c3fb5f84a792c14623037ded64edea46fca5d300
4SapH <skaterpotter@gmail.com>
2017-04-24 20:29:25 +0300

    Improved Mechanics
    Player Collision (hp), reloading.

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+	public int maxHp;
+	public int hp;
+
+	// Use this for initialization
+	void Start () {
+		maxHp = 3;
+		hp = maxHp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D obj) {
+		if (obj.gameObject.tag == "Enemy") {
+			hp--;
+			if (hp == 0){
+				Destroy(gameObject);
+			}
+		}
+	}
+}
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -7,20 +7,34 @@ public class PlayerShoot : MonoBehaviour {
 
 	private bool shot;
 	private float time;
-	private float interval;
+	private float sTime;
+	private float rTime;
+	
+	private float maxBullet;
+	private float curBullet;
 	// Use this for initialization
 	void Start () {
 		shot = false;
-		time =  0.7f;
-		interval = 0.7f;
+		time =  0.4f; // Current Interval
+		sTime = 0.4f; // Shooting Interval
+		rTime = 0.7f; // Reload Interval
+		maxBullet = 3;
+		curBullet = maxBullet;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		time += Time.deltaTime;
-		if (Input.GetKeyDown("space") && (time >= interval)){
-			Instantiate(bullet);
+		if (Input.GetKeyDown("space")){
+			if((time >= rTime && curBullet == maxBullet) || (time >= sTime && curBullet > 0 && curBullet < maxBullet)){
+				Instantiate(bullet);
+				time = Time.deltaTime;
+				curBullet--;
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.R)){
 			time = Time.deltaTime;
+			curBullet = maxBullet;
 		}
 	}
 }
